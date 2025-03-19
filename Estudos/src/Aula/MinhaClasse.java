cf8ef9527aa994237e6a24e3d7dd044300ea2ce8
renanvilabela <vilabelarenan@gmail.com>
2025-03-19 14:14:07 -0300

    realocando estudos java sobre anatomia de classes

--- Estudos/src/Aula/MinhaClasse.java
+++
@@ -1,7 +0,0 @@
-package Aula;
-
-public class MinhaClasse {
-	public static void main (String [] args) { 
-		System.out.print( "Dale rato" );
-	}
-}
---
+++ estudos-java/anatomiadeclasse/src/anatomiadeclasse/MinhaClasse.java
@@ -0,0 +1,16 @@
+package anatomiadeclasse;
+
+public class MinhaClasse {
+	public static void main (String [] args) {
+		String primeiroNome = "Renan";
+		String segundoNome = "Vila Bela";
+		
+		String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+		
+		System.out.println(nomeCompleto);
+	}
+	public static String nomeCompleto (String primeiroNome, String segundoNome) {
+		return "resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+	}
+}
+
